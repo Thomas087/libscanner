6ce7db9ff519bdf33a5801773e1406c4591bf349
Thomas Graziani <thomas.graziani@gmail.com>
2025-10-22 16:29:14 +0200

    Basculer sur postgres

--- libscanner/settings.py
+++ libscanner/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "libscanner.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "libscanner",
+        "USER": "thomas_duqi",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
